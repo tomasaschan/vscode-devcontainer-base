7c3ebfc06a47a21450d918e505acb5a6f33a8c69
Tomas Aschan <tomasl@spotify.com>
2021-03-08 14:42:42 +0100

    Add golang container

---
+++ golang/install-dev-tools.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+DEBIAN_FRONTEND=noninteractive
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]') $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+
+apt-get update 
+apt-get install -y \
+    bash-completion \
+    docker-ce-cli \
+    entr \
+    git \
+    gnupg2 \
+    httpie \
+    nano \
+    openssh-client \
+    rsync \
+    sudo \
+    unzip \
+    
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+curl -sSL -o /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/"$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"/bin/linux/amd64/kubectl
+chmod +x /usr/local/bin/kubectl
+
+curl https://raw.githubusercontent.com/microsoft/vscode-dev-containers/master/containers/kubernetes-helm/.devcontainer/copy-kube-config.sh -o /usr/local/share/copy-kube-config.sh
+
+apt-get autoremove -y 
+apt-get clean autoclean 
+rm -rf /var/lib/{apt,dpkg,cache,log} 
---
+++ golang/setup-user.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env sh
+
+set -e
+
+groupadd docker
+
+useradd -m dev -s /bin/bash
+echo "dev:changeme" | chpasswd
+usermod -aG docker dev
+echo "dev ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+mkdir -p /workspace && \
+chown -R dev:dev /workspace
+
+mkdir -p /home/dev/.bashrc.d
+# shellcheck disable=SC2016
+printf 'if [ -d $HOME/.bashrc.d/ ]; then
+  for rc in $HOME/.bashrc.d/*rc.sh; do
+    source $rc
+  done
+fi
+' >> /home/dev/.bashrc
+
+mkdir -p /home/dev/.local/bin
+# shellcheck disable=SC2016
+echo 'export PATH=$HOME/.local/bin:$PATH' > /home/dev/.bashrc.d/localbinrc.sh
+
+if [ -f /usr/local/share/copy-kube-config.sh ]; then
+  chown dev:root /usr/local/share/copy-kube-config.sh
+fi
+
+printf 'if [ -f /usr/local/share/copy-kube-config.sh ]; then
+  source /usr/local/share/copy-kube-config.sh
+fi
+
+if type "kubectl" > /dev/null 2>&1; then
+  source <(kubectl completion bash)
+
+  alias k=kubectl
+  complete -F __start_kubectl k
+fi
+' > /home/dev/.bashrc.d/kubectlrc.sh
